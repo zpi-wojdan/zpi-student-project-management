edaa2cacea7ce8c7c2a7d014f90c37a45bd558c1
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-09-06 20:05:58 +0200

    init commit

---
+++ zpi-backend/src/main/java/pwr/zpibackend/ZpiBackendApplication.java
@@ -0,0 +1,13 @@
+package pwr.zpibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZpiBackendApplication.class, args);
+	}
+
+}
---
+++ zpi-backend/src/test/java/pwr/zpibackend/ZpiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pwr.zpibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZpiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
