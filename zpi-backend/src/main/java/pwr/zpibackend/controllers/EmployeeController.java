ffc407e08c1b4e364b3ff1a68345f1185b82225e
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2023-10-23 15:30:49 +0200

    SCRUM-65-Dodanie-z-istniejacym-pracownikiem-error, z nowym-git, cos jest nie tak

--- zpi-backend/src/main/java/pwr/zpibackend/controllers/EmployeeController.java
+++ zpi-backend/src/main/java/pwr/zpibackend/controllers/EmployeeController.java
@@ -3,12 +3,11 @@ package pwr.zpibackend.controllers;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import pwr.zpibackend.models.Employee;
 import pwr.zpibackend.services.EmployeeService;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -21,5 +20,16 @@ public class EmployeeController {
     public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
         return new ResponseEntity<>(employeeService.addEmployee(employee), HttpStatus.CREATED);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable String id) {
+        return new ResponseEntity<>(employeeService.getEmployee(id), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
+    }
+
 
 }
