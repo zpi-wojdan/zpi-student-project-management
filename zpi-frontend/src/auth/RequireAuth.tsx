d903a182683e2e58273f6c22a279d32b899695dd
zsikorska <sikorska.zuzia@gmail.com>
2023-10-30 22:42:20 +0100

    SCRUM-183 Created alert for logout because of denied access

--- zpi-frontend/src/auth/RequireAuth.tsx
+++ zpi-frontend/src/auth/RequireAuth.tsx
@@ -2,18 +2,24 @@ import {Navigate, Outlet, useLocation} from "react-router-dom";
 import Cookies from "js-cookie";
 import {Role} from "../models/Role";
 import React from "react";
+import useAuth from "./useAuth";
 
 const RequireAuth: React.FC<{ allowedRoles: string[] }> = ({ allowedRoles }) => {
+    // @ts-ignore
+    const { auth, setAuth } = useAuth();
     const user = Cookies.get('user') ? JSON.parse(Cookies.get('user') as string) : '';
     const location = useLocation();
 
-    return (
-        user?.roles?.find((role: Role) => allowedRoles.includes(role.name)) || allowedRoles.includes(user?.role?.name)
-            ? <Outlet />
-            : user
-                ? <Navigate to="/unauthorized" state={{ from: location }} replace/>
-                : <Navigate to="/login" state={{ from: location }} replace/>
-    );
+    if(user?.roles?.find((role: Role) => allowedRoles.includes(role.name)) || allowedRoles.includes(user?.role?.name)){
+        return <Outlet />
+    }
+    else if(user){
+        return <Navigate to="/unauthorized" state={{ from: location }} replace/>
+    }
+    else{
+        setAuth({ ...auth, reasonOfLogout: 'access_denied' });
+        return <Navigate to="/login" state={{ from: location }} replace/>
+    }
 }
 
 export default RequireAuth
