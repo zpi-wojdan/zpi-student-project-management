5c0d6887ae166eb0e4436d3894e1948deaa70c9c
zsikorska <sikorska.zuzia@gmail.com>
2023-11-05 23:21:55 +0100

    SCRUM-102 Created test for employee controller

--- zpi-backend/src/main/java/pwr/zpibackend/exceptions/CannotDeleteException.java
+++ zpi-backend/src/main/java/pwr/zpibackend/exceptions/CannotDeleteException.java
@@ -1,6 +1,7 @@
 package pwr.zpibackend.exceptions;
 
 public class CannotDeleteException extends Exception{
+    public CannotDeleteException() {}
     public CannotDeleteException(String message) {
         super(message);
     }
