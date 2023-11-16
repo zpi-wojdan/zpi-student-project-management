68aa1a3c97ed4382f5c6f6031f716080c88c3cea
zsikorska <sikorska.zuzia@gmail.com>
2023-11-16 21:25:16 +0100

    SCRUM-205 Removed unused imports

--- zpi-backend/src/main/java/pwr/zpibackend/repositories/StudentRepository.java
+++ zpi-backend/src/main/java/pwr/zpibackend/repositories/StudentRepository.java
@@ -1,7 +1,6 @@
 package pwr.zpibackend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import pwr.zpibackend.models.Student;
 
