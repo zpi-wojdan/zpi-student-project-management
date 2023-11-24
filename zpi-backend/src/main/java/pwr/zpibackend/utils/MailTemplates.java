8d3bf0c59d749b61a5129e586cb032b47f7a4b83
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-11-24 17:19:30 +0100

    small fix

--- zpi-backend/src/main/java/pwr/zpibackend/utils/MailTemplates.java
+++ zpi-backend/src/main/java/pwr/zpibackend/utils/MailTemplates.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public enum MailTemplates {
     RESERVATION_LEADER("reservationByLeader", "Nowa rezerwacja", "New reservation"),
     RESERVATION_STUDENT("reservationByStudent", "Nowa rezerwacja", "New reservation"),
-    RESERVATION_SUPERVISOR("reservationBySupervisor", "Nowa rezerwacja", "New reservation"),
+    RESERVATION_SUPERVISOR("reservationBySupervisorOrAdmin", "Nowa rezerwacja", "New reservation"),
     RESERVATION_SENT_TO_SUPERVISOR("reservationSentToSupervisor", "Grupa zapisana na temat pracy dyplomowej", "Group assigned to thesis"),
     RESERVATION_CANCELED("reservationCanceled", "Rezerwacja anulowana", "Reservation canceled"),
     THESIS_ADDED("thesisAdded", "Nowy temat pracy dyplomowej", "New thesis added");
