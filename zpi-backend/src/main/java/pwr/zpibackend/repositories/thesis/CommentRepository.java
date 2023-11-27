61f5d25dd105b8c06b2d2559bfa44b12b734490f
zsikorska <sikorska.zuzia@gmail.com>
2023-11-27 15:07:02 +0100

    Change query annotation i comment repository to jpa syntax

--- zpi-backend/src/main/java/pwr/zpibackend/repositories/thesis/CommentRepository.java
+++ zpi-backend/src/main/java/pwr/zpibackend/repositories/thesis/CommentRepository.java
@@ -3,16 +3,11 @@ package pwr.zpibackend.repositories.thesis;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import pwr.zpibackend.models.thesis.Comment;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 @RepositoryRestResource
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    @Query("SELECT c FROM Comment c WHERE c.thesis.Id = :thesisId")
-    List<Comment> findAllByThesisId(@Param("thesisId") Long thesisId);
-
-    @Query("SELECT c FROM Comment c WHERE c.author.id = :authorId")
-    List<Comment> findAllByAuthorId(@Param("authorId") Long authorId);
+    List<Comment> findAllByThesisId(Long thesisId);
+    List<Comment> findAllByAuthorId(Long authorId);
 }
