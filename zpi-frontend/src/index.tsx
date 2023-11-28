30d8de260e251346667de2fdc2e10b73712ce4c2
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2023-11-28 17:45:08 +0100

    SCRUM-213-Fix-merge

--- zpi-frontend/src/index.tsx
+++ zpi-frontend/src/index.tsx
@@ -14,11 +14,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
       <I18nextProvider i18n={i18n}>
         <App />
       </I18nextProvider>
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
