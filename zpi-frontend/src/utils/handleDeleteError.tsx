8485a7a85b6a1343ffaa8fef21ba4eec44c57598
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-11-29 11:45:23 +0100

    fix dla wydziałów

--- zpi-frontend/src/utils/handleDeleteError.tsx
+++ zpi-frontend/src/utils/handleDeleteError.tsx
@@ -16,9 +16,32 @@ export const handleDeletionError = (error: any, t: TFunction<"translation", unde
                 tableNames.push(tableName);
             }
 
-            if (tableNames && tableNames.length === 3) {
+            if (tableNames.length === 3) {
                 const deletedTable = tableNames[0];
                 const referencedTable = tableNames[1];
+
+                const translatedReferencedTable = t(`tables.${referencedTable}`, { defaultValue: referencedTable });
+
+                const translatedMessage = t(`${deletedTable}.deleteErrorWithReference`, {
+                    referencedTable: translatedReferencedTable
+                });
+
+                toast.error(translatedMessage);
+                return;
+            }
+        } else if (errorMessage.includes('null value in column')) {
+            const relationRegex = /relation "(?<table>[a-zA-Z0-9_]+)"/mg;
+            const tableNames = [];
+            let match;
+
+            while ((match = relationRegex.exec(errorMessage)) !== null) {
+                const tableName = match.groups?.table;
+                tableNames.push(tableName);
+            }
+
+            if (tableNames.length === 1) {
+                const deletedTable = deletedFrom;
+                const referencedTable = tableNames[0];
 
                 const translatedReferencedTable = t(`tables.${referencedTable}`, { defaultValue: referencedTable });
 
