8f23f1a6890e8ebf5eebb509582df5bc5d8cc3a3
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-12-02 00:44:30 +0100

    ok maybe now?

--- zpi-frontend/src/utils/api_access.tsx
+++ zpi-frontend/src/utils/api_access.tsx
@@ -1,4 +1,4 @@
 
-var api_access = process.env.REACT_APP_BACKEND_HOST || "http://localhost:8080/";
+var api_access = process.env.REACT_APP_BACKEND_HOST || "http://localhost:8443/";
 
 export default api_access;
