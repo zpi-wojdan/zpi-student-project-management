a17022e0ec8037b9fccd3b14b843a95bb6aaa6cc
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-12-03 15:45:34 +0100

    lol

--- zpi-frontend/src/utils/api_access.tsx
+++ zpi-frontend/src/utils/api_access.tsx
@@ -1,4 +1,4 @@
 
-var api_access = process.env.REACT_APP_BACKEND_HOST || "https://proj01.zpi.wit.pwr.edu.pl:8443/";
+var api_access = process.env.REACT_APP_BACKEND_HOST || "https://proj01.zpi.wit.pwr.edu.pl:/api/";
 
 export default api_access;
